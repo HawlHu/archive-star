c668d6eb318826f97b406ff0c5ab447c8290991b
歸檔星球 <150411267+HawlHu@users.noreply.github.com>
2026-08-28 10:06:17 +0800

    Delete js directory

--- js/StrongPasswordGenerator.js
+++
@@ -1,8 +0,0 @@
-function SPG_GenPassword(lengthOfPassword=8, UseSymbols=true,HeadPassword="",theLetters="adehijnr",theNumber="34567",theSymbols="#@%+=.*") {
-	theLetters 		+=	theLetters.toUpperCase()+
-						theNumber+
-						(UseSymbols ? theSymbols : "");
-	for (var i = 0; i < lengthOfPassword; i++)HeadPassword += theLetters.charAt(Math.floor(Math.random() * theLetters.length));
-	return HeadPassword; 
-}
-
